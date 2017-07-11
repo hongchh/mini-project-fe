0a0e4bfa74bb1f5b3e22ef948b33127e5eb35ac7
hongchh <hongchh_sysu@qq.com>
2017-07-11 17:05:00 +0800

    完善部分页面

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,16 +1,21 @@
 <template lang="pug">
 div.main-page
   img(src="../assets/QQ.jpg")
-  button(@click="takePhoto") 拍照并上传
+  button(@click="takePhoto") {{ action }}
 </template>
 
 <script>
 export default {
   name: 'main-page',
+  data () {
+    return {
+      action: this.$route.query.error ? '照片无效请重拍' : '拍照并上传'
+    }
+  },
   methods: {
     takePhoto () {
       console.log('take photo')
-      // window.PhotoNative.takePhoto()
+      // TODO: 调用Native接口拍照并上传
     }
   }
 }
@@ -18,14 +23,15 @@ export default {
 
 <style lang="sass">
 .main-page
-  min-height: 100%
+  height: 100%
   display: flex
   flex-direction: column
   box-sizing: border-box
-  padding: .2rem .3rem
+  padding: .3rem
   img
     flex-grow: 1
     border: .01rem solid #888
+    max-height: 80%
   button
     width: 100%
     background: #12b7f5
