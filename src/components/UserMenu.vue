d5e9edc05cbd547e99807d494d26fa42a9a9bd9c
dan su <dansu@dandeMacBook-Air.local>
2017-07-11 16:10:33 +0800

    userMenu commit first

--- src/components/UserMenu.vue
+++ src/components/UserMenu.vue
@@ -1,42 +1,43 @@
 <template lang="pug">
-div(:class="isShowMenu ? 'user-menu showMenu' : 'user-menu'")
-	div(:class="isPrefer || isCount ? 'tab-wraper prefer' : 'tab-wraper'")
-		div.tab-1.tab
-			p(@click="hideMenu") 返回
-			ul
-				li(@click="setPrefer") 偏好设置
-				li(@click="userCount") 账户
-				li 分享给朋友
-			div(:class="isCount ? 'tab-2 tab prefer-hide' : 'tab-2 tab'")
-				p(@click="lastMenu") 返回
-				.info
-					div#title
-						span 为我匹配
-					div#age
-						span 年龄
-						div
-							vue-slider(v-model="ageRange")
-					div#sex
-						span 性别
-						div
-							span#boy(:class="sex === 0 ? 'blue-text' : ''", @click="sex = 0") 男 ♂
-							span#girl(:class="sex === 1 ? 'red-text' : ''", @click="sex = 1") 女 ♀
-			div(:class="isCount ? ' tab-3 tab tab3-show' : 'tab-3 tab'")
-				p(@click="lastMenu") 返回
-				p 账户
-				p 累计收入
-
+  div(:class="isShowMenu ? 'user-menu showMenu' : 'user-menu'")
+    div(:class="isPrefer || isCount ? 'tab-wraper prefer' : 'tab-wraper'")
+      div.tab-1.tab
+        p(@click="hideMenu") < 返回
+        ul
+          li(@click="setPrefer") 偏好设置
+          li(@click="userCount") 账户
+          li 分享给朋友
+      div(:class="isPrefer ? 'tab-2 tab' : 'tab-2 tab prefer-hide'")
+        p(@click="lastMenu") < 返回
+        .info
+          div#title
+            span 为我匹配
+          div#age
+            span 年龄
+            div
+              vue-slider(v-model="ageRange")
+          div#sex
+            span 性别
+            div
+              span#boy(:class="sex === 0 ? 'blue-text' : ''", @click="sex = 0") 男 ♂
+              span#girl(:class="sex === 1 ? 'red-text' : ''", @click="sex = 1") 女 ♀
+      div(:class="isCount ? ' tab-3 tab tab3-show' : 'tab-3 tab'")
+        p(@click="lastMenu") < 返回
+        div
+          span 账户
+          span.money {{countMoney}}
+        div
+          span 累计收入
+          span.money {{totalIncome}}
 </template>
 
 <script>
 import VueSlider from 'vue-slider-component'
-import UserMenu from './UserMenu'
 
 export default {
-  name: 'user-menu',
+  name: 'main-page',
   components: {
-    VueSlider,
-    UserMenu
+    VueSlider
   },
   props: {
     isShowMenu: false
@@ -46,14 +47,15 @@ export default {
       isPrefer: false,
       isCount: false,
       ageRange: [0, 100],
-      sex: -1
+      sex: -1,
+
+      countMoney: '￥' + 12.98,
+      totalIncome: '￥' + 52.88
     }
   },
   methods: {
-    showMenu () {
-      this.isShowMenu = !this.isShowMenu
-    },
     hideMenu () {
+      this.$emit('parentHideMenu', false)
       this.isShowMenu = false
     },
     setPrefer () {
@@ -63,82 +65,92 @@ export default {
       this.isCount = true
     },
     lastMenu () {
-      this.isPrefer = false
       this.isCount = false
+      this.isPrefer = false
     }
   }
 }
-
 </script>
+
 <style lang="sass">
-	.user-menu
-		position: absolute
-		top: 0
-		left: -5rem
-		overflow: hidden
-		width: 80%
-		font-size: .15rem
-		background: rgba(0, 0, 0, .6)
-		color: #ffffff
-		transition: all 1s ease-out
-		.tab-wraper
-			width: 200%
-			overflow: hidden
-			transition: all 1s ease-out
-			.tab
-				float: left
-				padding: 5%
-				width: 40%
-			.tab-3
-				position: absolute
-				right: 0
-				opacity: 0
-		 	.tab3-show
-				opacity: 1
-		.prefer
-			transform: translate(-50%, 0)
-		.showMenu
-			left: 0
-		.user-mini-photo
-			display: flex
-			justify-content: flex-start
-			overflow: hidden
-			width: 100%
-			height: .5rem
-			transition: all 1s ease-out
-			img
-			width: .5rem
-			border: 1px solid #eee
-			border-radius: 50%
-		.move-right
-			justify-content: flex-end
-	.info
-		flex-grow: 1
-		padding: 0 .2rem
-		display: flex
-		flex-direction: column
-		justify-content: center
-	#age, #sex, #title
-		padding: .1rem 0
-		border-bottom: .01rem solid #888
-		font-size: .18rem
-		display: flex
-		> span
-			margin-right: .2rem
-		> div
-	    	flex-grow: 1
-	#title span
-		font-size: .15rem
-	#sex, #age
-		margin-top: .35rem
-	#sex
-		> div
-			display: flex
-			justify-content: space-around
-		#boy, #girl
-			color: #888
-		#boy.blue-text
-			color: #25a4d4
-		#girl.red-text
-			color: #faa523
+ul 
+  list-style-type: square
+p
+  padding: 0
+  margin: 0
+
+.user-menu
+  position: absolute
+  top: 0
+  left: -800px
+  z-index: 999
+  overflow: hidden
+  width: 80%
+  font-size: .15rem
+  background: #000000
+  color: #ffffff
+  transition: all 1s ease-out
+  .tab-wraper
+    position: relative
+    width: 200%
+    height: 100%
+    overflow: hidden
+    transition: all .8s ease-out
+    .tab
+      float: left
+      padding: 5%
+      width: 40%
+    .tab-1
+      li
+        padding: .1rem
+      span 
+        display: inline-block
+        width: .03rem
+        height: .03rem
+        background: #ffffff
+    .tab-3
+      position: absolute
+      right: 0
+      display: none
+      div
+        padding: .2rem
+        .money
+          float: right
+    .tab3-show
+        display: block  
+  .prefer
+    transform: translate(-50%, 0)
+  .prefer-hide
+    display: none
+.showMenu
+  left: 0
+
+.info
+  flex-grow: 1
+  padding: 0 .2rem
+  display: flex
+  flex-direction: column
+  justify-content: center
+  #age, #sex, #title
+    padding: .1rem 0
+    border-bottom: .01rem solid #888
+    display: flex
+    > span
+      margin-right: .2rem
+    > div
+      flex-grow: 1
+  #title span
+    font-size: .15rem
+  #sex, #age
+    margin-top: .35rem
+  #sex
+    > div
+      display: flex
+      justify-content: space-around
+    #boy, #girl
+      color: #888
+    #boy.blue-text
+      color: #25a4d4
+    #girl.red-text
+      color: #faa523
 </style>
