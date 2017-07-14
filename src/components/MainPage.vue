49d83c8e71eb76e98b3f0e0bd6fbe4606a25e57f
su-dan <1197640676@qq.com>
2017-07-14 15:23:22 +0800

    update

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -5,27 +5,37 @@ div.main-page(@click.prevent="isShowMenu = false")
     img(:class="isShowMenu ? 'move-right' : ''" src="../assets/QQ.jpg" @click.stop="showMenu")
   //- img.user-photo(src="../assets/QQ.jpg")
   div.ad-container
-    .banner-wraper#banner-wraper(ref="bannerWraper" :style="{width: bannerWraperWidth}"
-    @touchstart="touchStartHandler"
-    @touchmove="touchmoveHandler"
-    @touchend="touchEndHandler")
-      .banner-1.banner
-      .banner-2.banner
-      .banner-3.banner
+    swiper(:options="swiperOption")
+      swiper-slide(v-for="slide in swiperSlides") 
+        img(src="../assets/tab1.jpeg")
+      div.swiper-pagination(slot="pagination")
   button(@click="takePhoto") {{ action }}
 </template>
 
 <script>
 import UserMenu from './UserMenu1'
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
+
 export default {
   name: 'main-page',
   components: {
-    UserMenu
+    UserMenu,
+    swiper,
+    swiperSlide
   },
   data () {
     return {
       action: this.$route.query.error ? '照片无效请重拍' : '拍照并上传',
-      isShowMenu: false
+      isShowMenu: false,
+      swiperOption: {
+        autoplay: 3500,
+        setWrapperSize: true,
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        mousewheelControl: true,
+        observeParents: true
+      },
+      swiperSlides: [1, 2, 3]
     }
   },
   methods: {
@@ -64,6 +74,10 @@ p
   .ad-container
     overflow: hidden
     height: 80%
+    width: 100%
+    img
+      width: 100%
+      height: 100%
   .user-mini-photo
     position: relative
     height: .5rem
