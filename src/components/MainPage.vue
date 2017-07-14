85a81094811944d509224fdeb5eb90d8ab742e59
su-dan <1197640676@qq.com>
2017-07-14 22:52:05 +0800

    update adpic size

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -117,15 +117,15 @@ p
         height: 100%
         width: 33.33333333%
       .banner-1
-        background: url("../assets/adver1.jpg")
-        background-size: 100% 100%
+        background: url("../assets/adver1.jpg") no-repeat
+        background-size: cover
       .banner-2
-        background: url("../assets/adver2.jpg")
-        background-size: 100% 100%
+        background: url("../assets/adver2.jpg") no-repeat
+        background-size: cover
       .banner-3
         position: relative
-        background: url("../assets/adver3.jpg")
-        background-size: 100% 100%
+        background: url("../assets/adver3.jpg") no-repeat
+        background-size: cover
     .ad-wraper
       height: 100%
     img
